0597ff203c8403848ad0849502aac18a26ed1144
Richie Reuben Hermanto <134058136+richiereubenn@users.noreply.github.com>
2024-05-04 14:03:02 +0700

    AI price plan page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('landing');
 });
+
+Route::get('/ai-price-plan', function () {
+    return view('ai-price-plan');
+});
