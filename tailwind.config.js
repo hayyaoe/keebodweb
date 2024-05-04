b93e82c742e8267616d946d883c9861db34ce2bf
hayyaoe <hayya947@gmail.com>
2024-05-04 12:30:37 +0700

    landing done

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,14 @@ export default {
       fontFamily: {
         archivo: ["Archivo Black", "sans-serif"],
       },
+      fontSize: {
+        "4.5xl": "2.5rem",
+        "7.5xl": "5rem",
+        "8.5xl": "6.25rem",
+        "10xl": "9rem",
+        "11xl": "12rem",
+        "12xl": "15rem",
+      },
     },
   },
   plugins: [],
