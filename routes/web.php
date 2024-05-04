75e23c50814430fc0f948ee1d375b2a56409521e
hayyaoe <hayya947@gmail.com>
2024-05-04 14:20:36 +0700

    custom keyboard page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Livewire\CustomKeyboard;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('landing');
+Route::get("/", function () {
+    return view("landing");
 });
+
+Route::get("/custom-keyboard", CustomKeyboard::class)->name("custom-keyboard");
