b6a4dd1ea393b8d1d6eb93c4e510fecd26b49069
hayyaoe <hayya947@gmail.com>
2024-05-08 02:30:04 +0700

    added google statistics

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -1,13 +1,21 @@
 <!doctype html>
 <html>
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-9CZ4Z05K1K"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-9CZ4Z05K1K');
+    </script>
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
-  
   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-  
   <link rel="stylesheet" type="text/css" href="{{ asset('css/lightbox.css') }}">
 
   <title>@yield('title')</title>
--- resources/views/layouts/livewirelayout.blade.php
+++ resources/views/layouts/livewirelayout.blade.php
@@ -1,6 +1,16 @@
 <!doctype html>
 <html>
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-9CZ4Z05K1K"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-9CZ4Z05K1K');
+    </script>
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
