5c28bd521b301d9c46ddd545dd7699c242edc080
Richie Reuben Hermanto <134058136+richiereubenn@users.noreply.github.com>
2024-05-08 01:12:30 +0700

    AI text area

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,21 @@ Route::get("/", function () {
     return view("landing");
 })->name("home");
 
+Route::get("/ai-input", function () {
+    return view("ai-input");
+})->name("ai-suggestion");
+
 Route::get("/ai-price-plan", function () {
     return view("ai-price-plan");
-})->name("ai-suggestion");
+})->name("ai-price-plan");
 
 Route::get("/prebuilt", function () {
     return view("prebuilt");
 })->name("prebuilt");
+
+Route::get("/coming-soon", function () {
+    return view("coming-soon");
+})->name("coming-soon");
+
 
 Route::get("/custom-keyboard", CustomKeyboard::class)->name("custom-keyboard");
